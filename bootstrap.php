e20925ee1106c4186e69b797a8afe6d5c5931843
KK161986 <kalaikannan.k@kgcas.com>
2024-06-21 16:47:59 +0530

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,7 @@
 
+<head>
+<title>Charles Severance - Rock Paper Scissors</title>
+
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
@@ -7,3 +10,21 @@
 
 <!-- Custom styles for this template -->
 <link href="starter-template.css" rel="stylesheet">
+
+</head>
+<body>
+<div class="container">
+<h1>Welcome to Rock Paper Scissors</h1>
+<p>
+<a href="login.php">Please Log In</a>
+</p>
+<p>
+Attempt to go to 
+<a href="game.php">game.php</a> without logging in - it should fail with an error message.
+</p><p>
+<a href="https://www.wa4e.com/assn/rps/" target="_blank">Specification for this Application</a>
+</p>
+</div>
+</body>
+
+
